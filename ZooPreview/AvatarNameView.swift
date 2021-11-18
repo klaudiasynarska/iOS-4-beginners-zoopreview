da5b7fc6fa79a4ce05053e5fef0b05dd5f46d31c
Klaudia Synarska <klaudiasynarska@wp.pl>
2021-11-18 22:16:56 +0100

    Added avatar and name views

---
+++ ZooPreview/AvatarNameView.swift
@@ -0,0 +1,34 @@
+//
+//  AvatarNameView.swift
+//  ZooPreview
+//
+//  Created by Klaudia Synarska on 18/11/2021.
+//
+
+import SwiftUI
+
+struct AvatarNameView: View {
+    
+    let name: String
+    let description: String
+    let avatarView: AvatarView
+    
+    var body: some View {
+        HStack {
+            avatarView
+            VStack(alignment: .leading) {
+                Text(name).font(.title)
+                Text(description)
+                    .foregroundColor(Color(UIColor.secondaryLabel))
+                    .font(.subheadline)
+                    
+            }
+        }
+    }
+}
+
+struct AvatarNameView_Previews: PreviewProvider {
+    static var previews: some View {
+        AvatarNameView(name: "Gotenks", description: "The escape goat", avatarView: AvatarView(emoji: "🐐"))
+    }
+}
---
+++ ZooPreview/AvatarView.swift
@@ -0,0 +1,30 @@
+//
+//  AvatarView.swift
+//  ZooPreview
+//
+//  Created by Klaudia Synarska on 18/11/2021.
+//
+
+import SwiftUI
+
+struct AvatarView: View {
+    
+    let emoji: String
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .foregroundColor(Color(UIColor.secondarySystemBackground))
+            Text(emoji)
+        }
+            .frame(width: 40, height: 40, alignment: .center)
+            .shadow(color: .gray, radius: 2, x: 0, y: 0)
+        
+    }
+}
+
+struct AvatarView_Previews: PreviewProvider {
+    static var previews: some View {
+        AvatarView(emoji: "🐐")
+    }
+}
