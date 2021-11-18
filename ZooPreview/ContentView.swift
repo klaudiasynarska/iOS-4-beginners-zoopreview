a1b401e3a3cd38ec404f884c778442e24561c719
Klaudia Synarska <klaudiasynarska@wp.pl>
2021-11-18 22:45:30 +0100

    Added avatar and name to content view

--- ZooPreview/ContentView.swift
+++ ZooPreview/ContentView.swift
@@ -12,16 +12,27 @@ struct ContentView: View {
 	let goat = goatGotenks
 	
     var body: some View {
-		ScrollView {
-			BouncingHeaderView {
-				Image(goat.mainImage)
-					.resizable()
-					.scaledToFill()
-			}
-			.frame(height: 300)
-			Text("Maaaaake me 🐐")
-				.padding()
-		}
+        ScrollView {
+            VStack(alignment: .leading, spacing: 0) {
+                BouncingHeaderView {
+                    Image(goat.mainImage)
+                        .resizable()
+                        .scaledToFill()
+                }
+                    .frame(height: 300)
+                
+                VStack(alignment: .leading) {
+                    AvatarNameView (
+                        name: goat.name,
+                        description: goat.subtitle,
+                        avatarView: AvatarView(emoji: "🐐" )
+                    )
+            
+                    Text("Maaaaake me 🐐")
+                }
+                .padding()
+            }
+        }
     }
 }
 
